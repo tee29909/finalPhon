f5fbe5316913f204c9eb992571717e560ca4fb45
tee29909 <59160571@go.buu.ac.th>
2019-10-16 17:54:42 +0700

    navigater

---
+++ app/src/main/java/buu/informatics/s59160571/random/home.kt
@@ -0,0 +1,26 @@
+package buu.informatics.s59160571.random
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class home : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+
+
+    }
+
+
+}
