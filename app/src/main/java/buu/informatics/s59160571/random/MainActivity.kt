6a9d69c36379e7e315a4af71b810b3b7c10e2311
tee29909 <59160571@go.buu.ac.th>
2019-11-08 19:39:22 +0700

    start fagment

--- app/src/main/java/buu/informatics/s59160571/random/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160571/random/MainActivity.kt
@@ -2,11 +2,20 @@ package buu.informatics.s59160571.random
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160571.random.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.Edit.setOnClickListener {
+
+        }
+        binding.buttonRandom.setOnClickListener {
+            
+        }
+        setContentView(R.layout.activity_main)
     }
 }
